fddfe83d23e65321ad99ff7b5cd17d8365d28e9e
Edward Khor <edward@enfonius.com>
2016-07-28 12:17:53 +0800

    react 0.15 fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var React = require('react/addons');
+var React = require('react');
 var TransitionGroup = React.createFactory(React.addons.TransitionGroup);
 
 var RAFList = require('./RAFList');
